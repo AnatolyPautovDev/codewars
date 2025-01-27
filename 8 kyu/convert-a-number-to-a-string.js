9f1b91e2130cc8cdcf69f53853a0c955249a1411
AnatolyPautovDev <pautov.tolick@yandex.by>
2025-01-27 21:44:14 +0200

    number to string

---
+++ 8 kyu/convert-a-number-to-a-string.js
@@ -0,0 +1,9 @@
+// ❓ DESCRIPTION:
+// We need a function that can transform a number (integer) into a string.
+// What ways of achieving this do you know?
+
+// ✅ SOLUTION:
+
+function numberToString(num) {
+    return String(num)
+}
