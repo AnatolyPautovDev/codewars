eb761687d785c1cafda54bdea15f678e775fca8b
AnatolyPautovDev <pautov.tolick@yandex.by>
2025-09-04 20:39:24 +0300

    Stop-gninnipS-My-sdroW

---
+++ 6 kyu/Array.diff.js
@@ -0,0 +1,11 @@
+// ❓ DESCRIPTION:
+
+//   Implement a function that computes the difference between two lists.
+//   The function should remove all occurrences of elements from the first list (a) that are present in the second list (b).
+//   The order of elements in the first list should be preserved in the result.
+
+// ✅ SOLUTION:
+
+function arrayDiff(a, b) {
+    return a.filter(e => !b.includes(e))
+}
