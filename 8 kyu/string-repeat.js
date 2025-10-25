deb51005cd4ad66ea29640ee3f7bf1d56fdefc68
AnatolyPautovDev <pautov.tolick@yandex.by>
2025-10-25 19:48:42 +0300

    string-repeat

---
+++ 8 kyu/string-repeat.js
@@ -0,0 +1,9 @@
+// ❓ DESCRIPTION:
+//   Write a function that accepts a non-negative integer n and a string s as parameters,
+//   and returns a string of s repeated exactly n times.
+
+// ✅ SOLUTION:
+
+function repeatStr (n, s) {
+  return s.repeat(n);
+}
