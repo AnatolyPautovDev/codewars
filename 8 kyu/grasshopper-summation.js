5238a106ec9742bde4d15e1333ceb4125f8bdfc5
AnatolyPautovDev <pautov.tolick@yandex.by>
2025-10-25 19:52:48 +0300

    grasshopper-summation

---
+++ 8 kyu/grasshopper-summation.js
@@ -0,0 +1,15 @@
+// ❓ DESCRIPTION:
+// Write a program that finds the summation of every number from 1 to num (both inclusive).
+// The number will always be a positive integer greater than 0.
+// Your function only needs to return the result, what is shown between parentheses in the example
+// below is how you reach that result and it's not part of it, see the sample tests.
+
+// ✅ SOLUTION:
+
+const summation = function (num) {
+  let result = 1
+  for (let i = 2; i<=num; i++) {
+    result += i
+  }
+  return result
+}
