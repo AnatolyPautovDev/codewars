159e80f1f4da41d924c2d6e217c95823d443bca5
AnatolyPautovDev <pautov.tolick@yandex.by>
2025-12-06 12:35:33 +0300

    returning-strings

---
+++ 8 kyu/returning-strings.js
@@ -0,0 +1,9 @@
+// ❓ DESCRIPTION:
+// Create a function that accepts a parameter representing a name and returns the message:
+// "Hello, <name> how are you doing today?".
+
+// ✅ SOLUTION:
+
+function greet(name){
+  return `Hello, ${name} how are you doing today?`
+}
