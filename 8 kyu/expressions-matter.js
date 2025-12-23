65b8d8083da640f29fe1e0f93082dd15230dac1d
AnatolyPautovDev <pautov.tolick@yandex.by>
2025-12-23 13:16:01 +0300

    expressions-matter

---
+++ 8 kyu/expressions-matter.js
@@ -0,0 +1,16 @@
+// ❓ DESCRIPTION:
+
+// Given three integers a, b, and c, return the largest number obtained after inserting the operators +, *, and parentheses ().
+// In other words, try every combination of a, b, and c with the operators, without reordering the operands, and return the maximum value.
+
+// ✅ SOLUTION:
+
+function expressionMatter(a, b, c) {
+  return Math.max(
+    a * (b + c),
+    a * b * c,
+    a + b * c,
+    (a + b) * c,
+    a + b + c
+  )
+}
