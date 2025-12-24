275117f4a22bc3d3de0e4f0f5dcb5edf1faa0270
AnatolyPautovDev <pautov.tolick@yandex.by>
2025-12-24 18:17:28 +0300

    are-the-numbers-in-order

---
+++ 7 kyu/are-the-numbers-in-order.js
@@ -0,0 +1,14 @@
+// ❓ DESCRIPTION:
+
+// In this Kata, your function receives an array of integers as input.
+// Your task is to determine whether the numbers are in ascending order.
+// An array is said to be in ascending order if there are no two adjacent integers where the left integer exceeds the right integer in value.
+
+// ✅ SOLUTION:
+
+function inAscOrder(arr) {
+ for (let i = 0; i < arr.length - 1; i++) {
+   if (arr[i] > arr[i+1]) return false
+ }
+ return true
+}
