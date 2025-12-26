da09cb9c5b8ac30ad0d93cdb775d0e84c06d460f
AnatolyPautovDev <pautov.tolick@yandex.by>
2025-12-26 14:57:21 +0300

    ones-and-zeros

---
+++ 7 kyu/ones-and-zeros.js
@@ -0,0 +1,10 @@
+// ❓ DESCRIPTION:
+
+// Given an array of ones and zeroes, convert the equivalent binary value to an integer.
+// Eg: [0, 0, 0, 1] is treated as 0001 which is the binary representation of 1.
+
+// ✅ SOLUTION:
+
+const binaryArrayToNumber = arr => {
+  return parseInt(arr.join(''), 2)
+};
